d8fc24184e548c4aef4d7b6f6b126a29b93cf680
Luiz-Gustavo-GIT <luizg.andrade7@gmail.com>
2024-09-20 17:15:21 -0300

    inicio

---
+++ Projeto01-React/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import PrimeiroComponente from './components/component1'
+
+function App() {
+  return (
+    <header className='headerPrincipal'>
+      <h1>Projeto 1</h1>
+      <PrimeiroComponente/>
+    </header>
+  )
+}
+
+export default App
---
+++ Projeto01-React/src/components/component1/index.jsx
@@ -0,0 +1,9 @@
+const PrimeiroComponente = () => {
+    return (
+        <div>
+            <p>Componente 1</p>
+        </div>
+    )
+}
+
+export default PrimeiroComponente
