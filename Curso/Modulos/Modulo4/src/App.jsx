ecc5b2a863862c0274f265cb11dd089296c2856b
Luiz-Gustavo-GIT <luizg.andrade7@gmail.com>
2024-09-24 11:18:05 -0300

    modulo4

---
+++ Curso/Modulos/Modulo4/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import MyComponent from './components/MyComponent'
+
+function App() {
+  const n = 4;
+  const redTitle = true;
+  return (
+    <div className='App'>
+      {/*css global*/}
+      <h1>React com CSS</h1>
+      {/* css de componente */}
+      <MyComponent />
+      <p>este paragrafo é do app.js</p>
+      {/* css inline */}
+      <p style={{color: "blue", padding:"25px", borderTop:"2px solid red"}}>elemento estilzado inline</p>
+      {/* CSS inline Dinamico */}
+      <h2 style={n < 10 ? ({color: 'purple'}) : ({color: 'pink'})}> CSS DINAMICO </h2>
+      {/* classe dinamica */}
+      <h2 className={redTitle ? "red-title" : "title"}>Este título tem classe dinamica</h2>
+    </div>
+  )
+}
+
+export default App
---
+++ Curso/Modulos/Modulo4/src/components/MyComponent.jsx
@@ -0,0 +1,14 @@
+import "./MyComponent.css"
+
+const MyComponent = () => {
+  return (
+    <div>
+       <h1>CSS de componente</h1>
+       <p>este é o paragrafo do componente</p>
+       <p className="paragrafoComponente">Esse é do componente e tem classe</p>
+    </div>
+    
+  )
+}
+
+export default MyComponent
