3ab49598e9ef2ee149380b4cea96ca25aeaa5eb3
Luiz-Gustavo-GIT <luizg.andrade7@gmail.com>
2024-10-01 16:57:42 -0300

    modulo7

--- Curso/Modulos/Modulo7/src/App.jsx
+++ Curso/Modulos/Modulo7/src/App.jsx
@@ -1,11 +1,73 @@
 import './App.css'
+import { useState, useEffect } from 'react'
+import { useFetch } from './hooks/useFetch'
+
+const url = 'http://localhost:3000/products'
 
 function App() {
+  const [products, setProducts] = useState([])
+
+  const {data: items} = useFetch(url)
+
+  const [name, setName] = useState("")
+  const [price, setPrice] = useState(0)
+
+  //resgatando dados
+
+  /*useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url)
+      const data = await res.json()
+      setProducts(data)
+    }
+    
+    fetchData()
+  }, [])*/
+
+  //adicionando dados
+  const handleSubmit = async(e) => {
+    e.preventDefault()
+
+    const product = {
+      name, 
+      price
+    }
+
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(product)
+    })
+
+    //carregamento dinamico
+    const addedProduct = await res.json()
+    setProducts((prevProducts) => [...prevProducts, addedProduct])
+    setName("")
+    setPrice(0)
+  }
 
   return (
     <>
       <div>
-        
+        <h1>Lista de produtos</h1>
+        <ul>
+          {items && items.map((product) => (
+            <li className='products' key={product.id}>{product.name} - R${product.price}</li>
+          ))}
+        </ul>
+        <div className="add-product">
+          <form onSubmit={handleSubmit}>
+            <label>
+              Nome: <input type="text" value={name} name="name" onChange={(e) => setName(e.target.value)}/>
+            </label>
+            <label>
+              Preço: <input type="text" value={price} name="price" onChange={(e) => setPrice(e.target.value)}/>
+            </label>
+            <input type="submit" value="criar"/>
+          </form>
+        </div>
       </div>
     </>
   )
