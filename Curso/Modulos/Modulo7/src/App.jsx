623632bce9454995d19beeb9042e8f968f2a067f
Luiz-Gustavo-GIT <luizg.andrade7@gmail.com>
2024-10-07 15:45:16 -0300

    modulo 7

--- Curso/Modulos/Modulo7/src/App.jsx
+++ Curso/Modulos/Modulo7/src/App.jsx
@@ -7,7 +7,7 @@ const url = 'http://localhost:3000/products'
 function App() {
   const [products, setProducts] = useState([])
 
-  const {data: items} = useFetch(url)
+  const {data: items, httpConfig, loading, error} = useFetch(url)
 
   const [name, setName] = useState("")
   const [price, setPrice] = useState(0)
@@ -27,13 +27,12 @@ function App() {
   //adicionando dados
   const handleSubmit = async(e) => {
     e.preventDefault()
-
     const product = {
       name, 
       price
     }
 
-    const res = await fetch(url, {
+    /*const res = await fetch(url, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -43,20 +42,29 @@ function App() {
 
     //carregamento dinamico
     const addedProduct = await res.json()
-    setProducts((prevProducts) => [...prevProducts, addedProduct])
+    setProducts((prevProducts) => [...prevProducts, addedProduct])*/
+
+    httpConfig(product, "POST")
     setName("")
     setPrice(0)
+  }
+
+  const handleRemove = async(id) => {
+    httpConfig(id, 'DELETE')
   }
 
   return (
     <>
       <div>
         <h1>Lista de produtos</h1>
-        <ul>
+        {/* loading */}
+        {error && <p>{error.message}</p>}
+        {loading && <p>Carregando dados...</p>}
+        {!loading && <ul>
           {items && items.map((product) => (
-            <li className='products' key={product.id}>{product.name} - R${product.price}</li>
+            <li className='products' key={product.id}>{product.name} - R${product.price} <button onClick={() => handleRemove(product.id)}>Remover</button></li>
           ))}
-        </ul>
+        </ul>}
         <div className="add-product">
           <form onSubmit={handleSubmit}>
             <label>
@@ -65,7 +73,9 @@ function App() {
             <label>
               Preço: <input type="text" value={price} name="price" onChange={(e) => setPrice(e.target.value)}/>
             </label>
-            <input type="submit" value="criar"/>
+            {/* state de loading no post */}
+            {!loading && <input type="submit" value="criar"/>}
+            {loading && <input type="submit" disabled value="aguarde"/>}
           </form>
         </div>
       </div>
